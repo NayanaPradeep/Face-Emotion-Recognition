79037c4e1f8382853c8ac1aec63144bf382114f9
Nayana Pradeep <nayanapradeep92@gmail.com>
2021-12-10 22:30:13 +0530

    Upload Files

---
+++ app.py
@@ -0,0 +1,101 @@
+# Importing Libraries
+import numpy as np
+import cv2
+import streamlit as st
+from tensorflow import keras
+from keras.models import load_model
+from keras.preprocessing.image import img_to_array
+from streamlit_webrtc import webrtc_streamer, VideoTransformerBase
+
+# load model
+emotion_dict = ["Angry","Disgust","Fear","Happy","Neutral","Sad","Surprise"]
+
+classifier =load_model('model1.h5')
+
+# load weights into new model
+classifier.load_weights("model1.h5")
+
+#load face
+try:
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+except Exception:
+    st.write("Error loading cascade classifiers")
+
+class VideoTransformer(VideoTransformerBase):
+    def transform(self, frame):
+        img = frame.to_ndarray(format="bgr24")
+
+        #image gray
+        img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(
+            image=img_gray, scaleFactor=1.3, minNeighbors=5)
+        for (x, y, w, h) in faces:
+            cv2.rectangle(img=img, pt1=(x, y), pt2=(
+                x + w, y + h), color=(255, 0, 0), thickness=2)
+            roi_gray = img_gray[y:y + h, x:x + w]
+            roi_gray = cv2.resize(roi_gray, (48, 48), interpolation=cv2.INTER_AREA)
+            if np.sum([roi_gray]) != 0:
+                roi = roi_gray.astype('float') / 255.0
+                roi = img_to_array(roi)
+                roi = np.expand_dims(roi, axis=0)
+                prediction = classifier.predict(roi)[0]
+                maxindex = int(np.argmax(prediction))
+                finalout = emotion_dict[maxindex]
+                output = str(finalout)
+            label_position = (x, y)
+            cv2.putText(img, output, label_position, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
+        return img
+
+def main():
+    # Face Analysis Application #
+    st.title("REAL TIME FACE EMOTION RECOGNITION")
+    ch_sel= ["Home", "Webcam Face Detection", "About"]
+    choice = st.sidebar.selectbox("Select", ch_sel)
+    st.sidebar.markdown(
+        """ DEVELOPED BY NAYANA PRADEEP""")
+    if choice == "Home":
+        html_temp_home1 = """<div style="background-color:#6D7B8D;padding:10px">
+                                            <h4 style="color:white;text-align:center;">
+                                            Face Emotion recognition using OpenCV, CNN model and Streamlit.</h4>
+                                            </div>
+                                            </br>"""
+        st.markdown(html_temp_home1, unsafe_allow_html=True)
+        st.write("""
+                 The application performs :
+
+                 1. Real time face detection using web cam feed.
+
+                 2. Real time face emotion recognition.
+                 """)
+    elif choice == "Webcam Face Detection":
+        st.header("Webcam Live Feed")
+        st.write("Click on start webcam and detect your emotion")
+        webrtc_streamer(key="example", video_transformer_factory=VideoTransformer)
+
+    elif choice == "About":
+        st.subheader("About this app")
+        html_temp_about1= """<div style="background-color:#6D7B8D;padding:10px">
+                                    <h4 style="color:white;text-align:center;">
+                                    </h4>
+                                    </div>
+                                    </br>"""
+        st.markdown(html_temp_about1, unsafe_allow_html=True)
+
+        html_temp4 = """
+                             		<div style="background-color:#98AFC7;padding:10px">
+                             		<h4 style="color:white;text-align:center;">This Application is developed
+                             		using Streamlit Framework, Opencv, Tensorflow and Keras library for demonstration purpose. </h4>
+                             		<h4 style="color:white;text-align:center;">Thanks for Visiting</h4>
+                             		</div>
+                             		<br></br>
+                             		<br></br>"""
+
+        st.markdown(html_temp4, unsafe_allow_html=True)
+    
+    else:
+        pass
+
+
+if __name__ == "__main__":
+    main()
